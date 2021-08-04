3298280b20f4879316c6fee26889118b4b3d09f0
hamedpa <hamedpa21@gmail.com>
2021-08-05 02:21:24 +0430

    fix som estuff

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<p>Hello, World!</p><img class="lnXdpd" alt="Google" height="92" src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" srcset="/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png 1x, /images/branding/googlelogo/2x/googlelogo_color_272x92dp.png 2x" width="272" data-atf="1" data-frt="0">'
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
     app.run()
