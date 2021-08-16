6b376240b36fc14119740992a734a5a507550cc0
hamedpa <hamedpa21@gmail.com>
2021-08-17 03:15:31 +0430

    added upload file and load N network model

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return '<p>Hello, World!</p><img class="logo" src="../_static/flask-icon.png" alt="Logo">'
+
+
+if __name__ == "__main__":
+    app.run()
