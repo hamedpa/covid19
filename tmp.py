62333b80d5de125858463a364ca082aebced34c7
hamedpa <hamedpa21@gmail.com>
2021-08-17 18:00:21 +0430

    added upload file and load N network model

--- tmp.py
+++ tmp.py
@@ -1,7 +1,30 @@
+from flask import Flask
+import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 import json
+
+import cv2
+import tensorflow as tf
+import numpy as np
 app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return '<p>Hello, World!</p><img class="logo" src="../_static/flask-icon.png" alt="Logo">'
+
+    
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# CATEGORIES = ["covid", "normal"]
+# #
+# def prepare(filepath):
+#     IMG_SIZE = 70
+#     img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+#     ret,thresh1 = cv2.threshold(img_array,85,255,cv2.THRESH_BINARY)
+#     thresh1 = thresh1/255.0
+
+#     new_array = cv2.resize(thresh1, (IMG_SIZE, IMG_SIZE))
+#     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
 UPLOAD_FOLDER = 'uploads_cnn'
@@ -13,7 +36,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=[ 'GET','POST'])
+@app.route('/', methods=[ 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -52,5 +75,6 @@ def upload_file():
       <input type=submit value=Upload>
     </form>
     '''
+
 if __name__ == "__main__":
     app.run()
